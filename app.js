602be636ee0e93a768614f18b8f6c5d38d085eb3
desutech <desaleyyazezew@gmail.com>
2025-01-01 14:01:07 +0300

    to deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const dotenv = require ('dotenv');
-      dotenv.config()
+      dotenv.config() 
 const express = require("express");
 const mysql2 = require ("mysql2");
 const db = require ("./db/dbConfig");
@@ -10,8 +10,10 @@ const authMiddle = require('./middleware/authMiddleware');
 
 const app = express();
 const port = 5501;
+
+
 const cors = require ("cors")
-app.use(cors())
+app.use(cors()) 
 
 app.use(express.json());
 app.use('/api/users', userRoute);
@@ -23,7 +25,7 @@ app.use("/api/answer", authMiddle, answerRoute);
 
 
 
-async function start() {
+async function start() { 
     try {
  const result = await db.execute ("select 'test' ");
          
